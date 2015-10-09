a1e05da8b1d018bcdd0e15ecb2af1d7e1b260631
Vagios Vlachos <bagios234@gmail.com>
2015-10-10 02:51:33 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# EU Cookie Law Banner
---
+++ eucookiecat.php
@@ -0,0 +1,239 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Eucookiecat extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'eucookiecat';
+        $this->tab = 'front_office_features';
+        $this->version = '0.1.0';
+        $this->author = 'Vagios Vlachos';
+        $this->need_instance = 0;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('EU Cookie Law Banner');
+        $this->description = $this->l('Show a banner to comply with EU cookie law');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall this module?');
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        // Set configuration values
+        Configuration::updateValue('EUCOOKIECAT_LIVE_MODE', false);
+
+        // Call parent and register hooks
+        if (!parent::isntall() ||
+            !$this->registerHook('displayHeader') ||
+            !$this->registerHook('backOfficeHeader') ||
+            !$this->registerHook('displayFooter') ||
+            !$this->registerHook('displayTop')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function uninstall()
+    {
+        // Delete configuration values
+        Configuration::deleteByName('EUCOOKIECAT_LIVE_MODE');
+
+        // Call parent
+        if (!parent::uninstall()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getHookController($hook_name)
+    {
+        // Include the controller file
+        require_once(dirname(__FILE__).'/controller/hook/'.$hook_name.'.php');
+
+        // Build the controller name
+        $controller_name = $this->name.$hook_name.'Controller';
+
+        // Instantiate controller
+        $controller = new $controller_name($this, __FILE__, $this->_path);
+
+        // Return the controller
+        return $controller;
+    }
+
+    public function getContent()
+    {
+        $controller = $this->getHookController('getContent');
+        return $controller->run();
+    }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        /**
+         * If values have been submitted in the form, process.
+         */
+        if (((bool)Tools::isSubmit('submitEucookiecatModule')) == true) {
+            $this->postProcess();
+        }
+
+        $this->context->smarty->assign('module_dir', $this->_path);
+
+        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+
+        return $output.$this->renderForm();
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitEucookiecatModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                'title' => $this->l('Settings'),
+                'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Live mode'),
+                        'name' => 'EUCOOKIECAT_LIVE_MODE',
+                        'is_bool' => true,
+                        'desc' => $this->l('Use this module in live mode'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Enabled')
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('Disabled')
+                            )
+                        ),
+                    ),
+                    array(
+                        'col' => 3,
+                        'type' => 'text',
+                        'prefix' => '<i class="icon icon-envelope"></i>',
+                        'desc' => $this->l('Enter a valid email address'),
+                        'name' => 'EUCOOKIECAT_ACCOUNT_EMAIL',
+                        'label' => $this->l('Email'),
+                    ),
+                    array(
+                        'type' => 'password',
+                        'name' => 'EUCOOKIECAT_ACCOUNT_PASSWORD',
+                        'label' => $this->l('Password'),
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+        return array(
+            'EUCOOKIECAT_LIVE_MODE' => Configuration::get('EUCOOKIECAT_LIVE_MODE', true),
+            'EUCOOKIECAT_ACCOUNT_EMAIL' => Configuration::get('EUCOOKIECAT_ACCOUNT_EMAIL', 'contact@prestashop.com'),
+            'EUCOOKIECAT_ACCOUNT_PASSWORD' => Configuration::get('EUCOOKIECAT_ACCOUNT_PASSWORD', null),
+        );
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+        $form_values = $this->getConfigFormValues();
+
+        foreach (array_keys($form_values) as $key) {
+            Configuration::updateValue($key, Tools::getValue($key));
+        }
+    }
+
+    /**
+    * Add the CSS & JavaScript files you want to be loaded in the BO.
+    */
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path.'views/js/back.js');
+            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/front.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+    }
+
+    public function hookDisplayFooter()
+    {
+        /* Place your code here. */
+    }
+
+    public function hookDisplayTop()
+    {
+        /* Place your code here. */
+    }
+}
